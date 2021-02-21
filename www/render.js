d6d22fb3d2617ae876f3ccbb75553074ba1f6609
asuradev99 <ethanaditya@gmail.com>
2021-02-20 19:18:16 -0700

    added batch renderer, currently broken

--- www/render.js
+++ www/render.js
@@ -1,87 +1,54 @@
-import * as PIXI from "pixi.js"
-import {Viewport} from "pixi-viewport"
-
-const app = new PIXI.Application({
-	autoResize: true,
-    resolution: devicePixelRatio, 
-    transparent: true
-});
-
-document.body.appendChild(app.view);
-
-window.addEventListener('resize', resize);
-
-
-const viewport = new Viewport({
-    screenWidth: window.innerWidth,
-    screenHeight: window.innerHeight,
-    worldWidth: 1000,
-    worldHeight: 1000,
-
-    interaction: app.renderer.plugins.interaction // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
-})
-
-app.stage.addChild(viewport)
-viewport
-    .drag()
-    .pinch()
-    .wheel()
-    .decelerate()
-
-// add a red box
-let container =  new PIXI.ParticleContainer({
-    scale: true,
-    uve: true, 
-    alpha: true
-}); 
-//viewport.addChild(container);
-viewport.addChild(container);
-var graphics = new PIXI.Graphics();
-graphics.beginFill(0xFFF);
-graphics.drawCircle(1, 1, 1);
-graphics.endFill();
-var texture = app.renderer.generateTexture(graphics);
-
-var graphics2 = new PIXI.Graphics();
-graphics2.beginFill(0xFFFF);
-graphics2.drawCircle(1, 1, 1);
-graphics2.endFill();
-var texture2 = app.renderer.generateTexture(graphics2);
-let sprites = [];
-for (var j = 0; j < 1000; j++) {
-    for (var i = 0; i < 1000; i++) {
-        //for (var j = 0; i < 10; j++) {
-            const sprite = container.addChild(new PIXI.Sprite(texture2))
-           // sprite.tint = Math.random() * 1000;
-            sprite.width = sprite.height = 1
-            sprite.position.set(i * 0.5 , j * 0.5)
-            sprite.interactive=true;
-            //sprite.on('click', () => {
-            //    sprite.tint = 0xffffff;
-            //});            
-            sprites.push(sprite);
-            //console.log(i,j)
-        //}
+class Renderer {
+    constructor(ctx, cellsize, width, height, nodes, states) {
+        this.ctx = ctx;
+        this.cellsize = cellsize;
+        this.ctx.width = width; 
+        this.ctx.height = height; 
+        this.nodes = nodes; 
+        this.states = states; 
+        this.data, this.delta;
+        console.log(this.states)
     }
-}
+    setMemory(memory, dataptr, deltaptr) {
+        this.data = new Uint8Array(memory.buffer, dataptr, this.width * this.height);
+        this.delta = new Uint32Array(memory.buffer, deltaptr, this.width * this.height);
+    }
+    nodeRender(begin, end) {
+        const node = document.createElement('canvas')
+        const nctx = node.getContext('2d');
+        this.states.forEach(s => {
+            console.log(s)
+            const value = s[0];
+            const color = s[1];
+            nctx.beginPath();
+            nctx.fillStyle = color;
+            for (let i = begin; i < end; i++) {
+              if (this.delta[i] < this.delta[i - 1]) {
+                //console.log("exited at ", i, ", ", this.delta[i - 1])
+                break;
+              }
+              let idx = this.delta[i];
+              if (this.data[idx] !== value) {
+                continue;
+              }
+              let row = Math.floor(idx / width);
+              let col = idx % width;
+              nctx.fillRect(
+                col * (this.cellsize + 1) + 1,
+                row * (this.cellsize + 1) + 1,
+                this.cellsize,
+                this.cellsize
+              );
+            }
 
-let tick = 0;
-app.ticker.add(() => {
-    for (let i = 0; i < sprites.length; i++) {
-        const sprite = sprites[i];
-        if (Math.random() < 0.5) {
-            sprite.texture = texture;
-        } else {
-            sprite.texture = texture2;
+        });
+        nctx.stroke();
+        return node;
+    };
+    batchRender() {
+        for (var i = 0; i < this.nodes; i++) {
+            var interval = (this.width * this.height) / this.nodes;
+            this.ctx.drawImage(this.nodeRender(interval * i, interval * (i + 1)), 0, 0);
         }
     }
-
-    tick += 1;
-})
-sprites[1]
-function resize() {
-	app.renderer.resize(window.innerWidth, window.innerHeight);
-    viewport.resize(window.innerWidth, window.innerHeight, viewport.worldWidth, viewport.worldHeight)
 }
-
-resize();
